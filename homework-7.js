b4a93e9d07c51290787cc35e18eb76391c9adc15
khbdev1 <halikovbulat2001@gmail.com>
2026-03-29 22:25:46 +0400

    исправление ошибок 1

--- homework-7.js
+++ homework-7.js
@@ -8,53 +8,49 @@ const newNumbers = numbers.filter(num => num >= 5);
 // 3. Проверка, есть ли в массиве какая-то определенная сущность
 
 const cars = ['BMW', 'Mercedes-Benz', 'Toyota', 'LADA'];
-const newArray = cars.includes('LADA');
+const hasLada = cars.includes('LADA');
 
 // 4. Функция, переворачивающая массив
 
 const getReverseArray = (array) => {
-  return array = array.reverse();
+  array.reverse();
 }
 numbers = getReverseArray(numbers);
 numbers = getReverseArray(cars);
 
 // 7. Вывод в консоль массив тех комментариев, почта пользователей которых содержит ".com"
 
-const comEmailsComments = comments.filter(comment => comment.email.includes('.com'));
-console.log(comEmailsComments);
+const commentEmails = comments.filter(comment => comment.email.includes('.com'));
+console.log(commentEmails);
 
 // 8. Перебор по id
 
-const newComments = comments.map(comment => {
-  if (comment.id <= 5) {
-    return {...comment, postId: 2};
-  } else {
-    return {...comment, postId: 1};
-  }
+const updatedComments = comments.map(comment => {
+  return {...comment, postId: comment.id <= 5 ? 2 : 1}
 });
 
 // 9. Перебор массива так, что бы объекты состояли только из айди и имени
 
-const idNamesOnly = comments.map(comment => {
+const commentNames = comments.map(comment => {
   return {id: comment.id, name: comment.name};
 })
 
 // 10. Добавление объектам свойства isInvalid
 
-const commentsWithInvalid = comments.map(comment => {
+const validatedComments = comments.map(comment => {
   return {...comment, isInvalid: comment.body.length > 180};
 })
 
 // 11. Вывод массива почт с помощью методов .reduce и .map
 
-const emails = comments.reduce((acc, comment) => {
+const emailsByReduce = comments.reduce((acc, comment) => {
   acc.push(comment.email);
   return acc;
 }, []);
 
-const emailsWithMap = comments.map(comment => comment.email);
+const emailsByMaps = comments.map(comment => comment.email);
 
 // 12. .toString и .join
 
-const toStringEmails = emailsWithMap.toString();
-const joinEmails = emailsWithMap.join();
+const toStringEmails = emailsByMaps.toString();
+const joinEmails = emailsByMaps.join();
